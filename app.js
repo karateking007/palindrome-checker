1930fc5c31c0368b371bb5a694d29e08612e07cb
Riley <rileylitchfield95@gmail.com>
2022-11-14 18:52:27 -0600

    Cleaned up code

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 let palindrome = (str) => {
-    const regExp = new RegExp('[A-Za-z0-9]', 'g');
-    let match = str.toLowerCase().match(regExp);
-    let reverseArr = [];
+    let match = str.toLowerCase().match(/[a-z0-9]/g);
 
-    for (let i = match.length - 1; i >= 0; i--) {
-        reverseArr.push(match[i]);
-    }
-
-    if (reverseArr.toString() == match.toString()) {
-        return true
-    } else {
-        return false
-    }
+    return match.join('') == match.reverse().join('');
 }
 
 palindrome("1 eye for of 1 eye.");
